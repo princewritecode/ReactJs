fe729576dfc23b17bd5f9088b93908ea637a309c
princewritecode <princewritecode@gmail.com>
2023-08-27 11:10:18 +0530

    React js food app added

--- episode4FoodApp/src/components/Body.js
+++ episode4FoodApp/src/components/Body.js
@@ -44,3 +44,4 @@ const Body = () => {
     );
 };
 export default Body;
+
--- episode4FoodApp/src/components/RestaurantCard.js
+++ episode4FoodApp/src/components/RestaurantCard.js
@@ -17,7 +17,7 @@ const RestaurantCard = ({
         <div className="card">
             <img src={IMG_CDN_URL + cloudinaryImageId} />
             <h2>{name}</h2>
-            <h5>{cuisines.join(", ")}</h5>
+            <h5>{cuisines.join(",")}</h5>
             <h6>{area}</h6>
             <span>
                 <h4
